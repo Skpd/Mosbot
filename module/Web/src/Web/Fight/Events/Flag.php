f90b5bc489a0a291fba6499c8f723cbc68371aa7
Skpd <dm.skpd@gmail.com>
2014-01-20 11:59:50 +0400

    updated flag event

--- module/Web/src/Web/Fight/Events/Flag.php
+++ module/Web/src/Web/Fight/Events/Flag.php
@@ -8,7 +8,7 @@ class Flag
 {
     function __invoke(Event $evt)
     {
-        if (stristr($evt->getAction()->textContent, 'сохраняет флаг')) {
+        if (stristr($evt->getAction()->textContent, 'сохраняет флаг') || stristr($evt->getAction()->textContent, 'перехватывает флаг')) {
             $evt->stopPropagation();
             return __CLASS__;
         }
